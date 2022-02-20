7440548ebf82776b88fcc52cd83dcdef4b786a13
Artemis <62397532+ArtemisRS@users.noreply.github.com>
2022-02-20 16:16:02 -0500

    Work on build times a bit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 
+use anyhow::Result;
 use randomize::{formulas::f32_half_open_left, RandRangeU32, PCG32};
 use serde::Deserialize;
 use std::fs;
@@ -11,11 +12,13 @@ mod simulator;
 
 #[derive(Debug, Deserialize)]
 struct Config {
+
     name: String,
-    //rotation: Option<zulrah::Rot>,
+    //future options
+    //rotation: Option<zulrah::Rot>, //specify a specific rotation
+    //beat_time: Option<u8>, //in ticks, for $ of kills faster than time
     //stall: Option<bool>,
     //attacks: Option<Vec<setup-ids>>, //list out every attack
-    //beat_time: Option<u8>, //in ticks
 
     serpentine: Setup,
     crimson: Setup,
@@ -30,13 +33,12 @@ pub struct Setup {
 }
 
 
-fn main() {
-    println!("Hello, world!");
-
+fn main() -> Result<()> {
     let input = fs::read_to_string("config").unwrap_or_else(|_|"Missing config file?".to_string());
 
-    let config: Config = toml::from_str(&input).unwrap();
+    let config: Config = toml::from_str(&input)?;
 
     dbg!(&config);
     simulator::main(&config.serpentine, &config.crimson, &config.tanzanite);
+    Ok(())
 }
